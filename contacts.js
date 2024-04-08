e7dbd93776f951998694af859918798b436ecbff
Michael Caceres <63517857+michaeling10@users.noreply.github.com>
2024-04-07 23:18:17 -0500

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,29 +1,24 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-// Ruta al archivo de contactos
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-// Función para cargar los contactos
 async function loadContacts() {
   const data = await fs.readFile(contactsPath, "utf8");
   return JSON.parse(data);
 }
 
-// Listar todos los contactos
 async function listContacts() {
   const contacts = await loadContacts();
   console.table(contacts);
 }
 
-// Obtener un contacto por ID
 async function getContactById(contactId) {
   const contacts = await loadContacts();
   const contact = contacts.find((contact) => contact.id === contactId);
   console.log(contact);
 }
 
-// Eliminar un contacto
 async function removeContact(contactId) {
   const contacts = await loadContacts();
   const filteredContacts = contacts.filter(
@@ -33,7 +28,6 @@ async function removeContact(contactId) {
   console.log(`Contact with ID ${contactId} has been removed.`);
 }
 
-// Agregar un contacto
 async function addContact(name, email, phone) {
   const contacts = await loadContacts();
   const newContact = {
